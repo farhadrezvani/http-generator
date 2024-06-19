2fa0cd8895e5f1bcc56273450f8f7958ee48ee7a
Farhad Rezvani <4rezvani@gmail.com>
2024-06-19 17:14:16 +0330

    Add tsup config

---
+++ src/cli.ts
@@ -0,0 +1,51 @@
+#!/usr/bin/env node
+
+import { Command } from "commander";
+import { convertToHttpFile, loadOpenAPISpec } from "./transforms";
+import { handleError, packageInfo, validateOptions } from "./utils";
+
+const { name, version } = packageInfo();
+const program = new Command();
+
+program
+  .option("-i, --input <input>", "OpenAPI specifications file or URL")
+  .option("-o, --output <output>", "Output directory or HTTP file")
+  .option("-b, --base-url <baseUrl>", "Base URL for the API")
+  .option("-t, --token <token>", "Authorization token")
+  .option(
+    "-s, --skip-validation",
+    "Skip validation of OpenAPI Specification",
+    false
+  )
+  .name(name)
+  .version(version, "-v, --version", "Display version number")
+  .helpOption("-h, --help", "Display this message")
+  .description("Generate .http files from OpenAPI specifications");
+
+program.parse(process.argv);
+
+if (process.argv.length <= 2) {
+  program.help();
+}
+
+async function main() {
+  const options = program.opts();
+
+  try {
+    validateOptions(options);
+    const openAPISpec = await loadOpenAPISpec(
+      options.input,
+      options.skipValidation
+    );
+    convertToHttpFile(
+      openAPISpec,
+      options.output,
+      options.baseUrl,
+      options.token
+    );
+  } catch (error) {
+    handleError(error);
+  }
+}
+
+main().catch(console.error);
